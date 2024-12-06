3659f158703297086545f45ef3d98585758568b3
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 15:04:15 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,21 @@
+/*
+*****
+**** 
+***  
+**   
+*   
+*/
+import java.util.*;
+public class Pattern4{
+    public static void main(String[]args){
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
