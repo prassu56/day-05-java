9d9f95362904df8fd272fe959bb8df1b9b196254
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 15:22:30 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,23 @@
+/*
+5 4 3 2 1 
+4 3 2 1   
+3 2 1     
+2 1       
+1        
+*/
+import java.util.*;
+public class Pattern9{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
