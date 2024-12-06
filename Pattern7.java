30fdb18cd068a235c6f7fcc246f8df981568009e
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 15:14:59 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,21 @@
+/*
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+*/
+import java.util.*;
+public class Pattern7{
+    public static void main(String[]args){
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=5;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
